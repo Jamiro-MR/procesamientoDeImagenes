60c3c75912c42cda2723eb49f44e7616e765d3ba
Roberto <111545434+Jamiro-MR@users.noreply.github.com>
2023-02-09 11:05:25 -0600

    Rotation by adjusting the matrix

--- practice1Checkerboard/rotateimg.py
+++ practice1Checkerboard/rotateimg.py
@@ -1,12 +1,17 @@
 import cv2
-import numpy
+import numpy as np
 
-img = cv2.imread("Tigre.jpg", cv2.IMREAD_UNCHANGED)
-cv2.imshow("Mexico desperto", img)
+img = cv2.imread("Tigre.jpg")
+
+cv2.imshow("Tigre despiertado", img)
 
 num_rows, num_cols = img.shape[:2]
 rotation_matrix = cv2.getRotationMatrix2D((num_cols/2, num_rows/2), -45, 1)
-img2 = cv2.warpAffine(img, rotation_matrix, (num_cols, num_rows))
-cv2.imshow("EL tigre mas rotado", img2)
+translation_matrix = np.float32([[-1, 0, 960], [0, 1, 0]])
+
+img2 = cv2.warpAffine(img, translation_matrix, (num_cols, num_rows))
+#img2 = cv2.warpAffine(img, rotation_matrix, (num_cols, num_rows))
+
+cv2.imshow("tigre volteado", img2)
 
 cv2.waitKey(0)
