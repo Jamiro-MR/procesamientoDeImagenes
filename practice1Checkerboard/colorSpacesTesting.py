fe314046d13ff83cf0164fcdb63974d56360cf18
Roberto <111545434+Jamiro-MR@users.noreply.github.com>
2023-02-16 11:36:13 -0600

    plotting

--- practice1Checkerboard/colorSpacesTesting.py
+++ practice1Checkerboard/colorSpacesTesting.py
@@ -2,22 +2,26 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread("Tigre.jpg", cv2.IMREAD_COLOR)
+img = cv2.imread("Tigre.jpg", cv2.IMREAD_UNCHANGED)
 img_reverso = img[:,:,::-1] #Invierte los colores de BGR a RGB
 
 #Conversión a YUV
-#img_yuv = cv2.cvtColor(img_reverso, cv2.COLOR_BGR2YUV)
+img_yuv = cv2.cvtColor(img_reverso, cv2.COLOR_BGR2YUV)
 #plt.imshow(img_yuv)
 
 #Conversión a HSV
-#img_hsv = cv2.cvtColor(img_reverso, cv2.COLOR_BGR2HSV)
+img_hsv = cv2.cvtColor(img_reverso, cv2.COLOR_BGR2HSV)
 #plt.imshow(img_hsv)
 
 #Conversión a YCrCb
-img_ycrcb = cv2.cvtColor(img_reverso, cv2.COLOR_RGB2YCrCb)
+img_YCrCb = cv2.cvtColor(img_reverso, cv2.COLOR_BGR2YCrCb)
+
 plt.figure(figsize=[10, 5])
-plt.subplot(11);plt.imshow(img_reverso);plt.title("Tigre toño rgbeado");
-plt.subplot(12);plt.imshow(img_ycrcb);plt.tittle("Tigre en el formato chistoso");
+
+plt.subplot(221);plt.imshow(img_reverso);plt.title("Tigre toño RGBeado");
+plt.subplot(222);plt.imshow(img_YCrCb);plt.title("Tigre toño formato curioson");
+plt.subplot(223);plt.imshow(img);plt.title("Tigre toño");
+plt.subplot(224);plt.imshow(img_yuv);plt.title("Tigre toño yuveado");
 
 #plt.imshow(img_ycrcb)
 #plt.title('Tigre toño yuveado')
