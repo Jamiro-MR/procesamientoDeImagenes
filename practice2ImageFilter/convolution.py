2adae6b9904114e9fd4fb3121b4cd02685a6e8df
Roberto <111545434+Jamiro-MR@users.noreply.github.com>
2023-02-21 09:49:56 -0600

    Convolution (Img filter)

---
+++ practice2ImageFilter/convolution.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread("Tigre.jpg", cv2.IMREAD_UNCHANGED)
+img_reverso = img[:,:,::-1] #Invierte los colores de BGR a RGB
+
+ksize = (3,3)
+img_blur = cv2.blur(img_reverso, ksize)
+
+#Conversión a YUV
+img_yuv = cv2.cvtColor(img_reverso, cv2.COLOR_BGR2YUV)
+#plt.imshow(img_yuv)
+
+#Conversión a HSV
+img_hsv = cv2.cvtColor(img_reverso, cv2.COLOR_BGR2HSV)
+#plt.imshow(img_hsv)
+
+#Conversión a YCrCb
+img_YCrCb = cv2.cvtColor(img_reverso, cv2.COLOR_BGR2YCrCb)
+
+plt.figure(figsize=[10, 5])
+
+plt.subplot(221);plt.imshow(img_reverso);plt.title("Tigre toño RGBeado");
+plt.subplot(222);plt.imshow(img_YCrCb);plt.title("Tigre toño formato curioson");
+plt.subplot(223);plt.imshow(img_yuv);plt.title("Tigre YUVEADO");
+plt.subplot(224);plt.imshow(img_blur);plt.title("Tigre toño con cataratas");
+
+#plt.imshow(img_ycrcb)
+#plt.title('Tigre toño yuveado')
+plt.show()
