8983f9228e0f45e09f2d538ba5ca2a5b330cedfc
Roberto <111545434+Jamiro-MR@users.noreply.github.com>
2023-03-14 08:06:02 -0600

    cambios

--- practice3HaarCascade/haar_cascades.py
+++ practice3HaarCascade/haar_cascades.py
@@ -1,16 +1,15 @@
 import cv2
 
-filename = "multitud.jpg"
+filename = "grupo.jpg"
 
 def detect(filename):
-	face_cascade = cv2.CascadeClassifier("/cascades/haarcascade_frontalface_default.xml")
-
-img = cv2.imread(filename)
-gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-	img = cv2.rectangle(img(x,y),(x+w, y+h),(255,0,0),2)
-	cv2.namedWindow("omg jetas detectadas")
+	face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+	img = cv2.imread(filename)
+	gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, 1.3, 1)
+	for (x,y,w,h) in faces:
+		img = cv2.rectangle(img,(x,y),(x+w, y+w),(255,0,0),2)
+		cv2.namedWindow("omg jetas detectadas")
 	cv2.imshow("omg jetas detectadas", img)
 	cv2.imwrite("multitud chistosona.jpg", img)
 	cv2.waitKey(0)
