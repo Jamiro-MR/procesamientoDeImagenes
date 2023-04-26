602c9fc7ed8474f583c66a2114b81e904d28face
Roberto <111545434+Jamiro-MR@users.noreply.github.com>
2023-04-26 10:18:00 -0600

    test

--- skimage/practica1Skimage.py
+++ skimage/practica1Skimage.py
@@ -1,1 +1,29 @@
 from skimage.io import imread
+from sklearn import datasets, svm
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+#Codigo para cargar la base de datos MNISTQ
+digits = datasets.load_digits()
+_,axes = plt.subplots(nrows=1, ncols=10, figsize=(10,3))
+for ax, image, label in zip(axes, digits.images, digits.target):
+	ax.set_axis_off()
+	ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+	ax.set_title("Entrenamiento: %i" % label)
+plt.show()
+
+#Codigo para entrenamiento
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1))
+clf = svm.SVC(gamma=0.001)
+X_train, X_test, y_train, y_test = train_test_split(data, digits.target, test_size=0.8, shuffle=False)
+
+clf.fit(X_train, y_train)
+predicted = clf.predict(X_test)
+_,axes = plt.subplots(nrows=1, ncols=9, figsize=(10,3))
+for ax, image, prediction in zip(axes, X_test,predicted):
+	ax.set_axis_off()
+	image = image.reshape(8,8)
+	ax.imshow(image, cmap=plt.cm.gray_r,interpolation="nearest")
+	ax.set_title(f"Prediction: {prediction}")
+plt.show()
